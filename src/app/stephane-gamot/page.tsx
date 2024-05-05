7e42aceaee2203d809105761caab0c61022df89c
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-05-05 08:04:30 +0200

    Update page.tsx

--- src/app/stephane-gamot/page.tsx
+++ src/app/stephane-gamot/page.tsx
@@ -38,104 +38,104 @@ export default function Page() {
 							<FaHtml5 className=" text-red-500 text-[40px]" />
 							<ProgressBar color="bg-red-500" percentage={100} />
 						</div>
-
-						<div className="flex max-w-[350px] mx-auto my-4">
+						
+						<div className="flex w-[350px] mx-auto my-4">
 							<FaCss3Alt className=" text-blue-500 text-[40px]" />
 							<ProgressBar color="bg-blue-500" percentage={100} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<FaReact className=" text-cyan-500 text-[40px]" />
 							<ProgressBar color="bg-cyan-500" percentage={100} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<SiNextdotjs className=" text-white text-[40px]" />
 							<ProgressBar color="bg-white" percentage={100} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<SiJavascript className=" text-yellow-300 text-[40px]" />
 							<ProgressBar color="bg-yellow-300" percentage={90} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<Image src={RestApi} alt="" width={40} height={40} className="bg-white rounded" />
 							<ProgressBar color="bg-white" percentage={90} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<Image src={Seo} alt="" width={40} height={40} className=" rounded" />
 							<ProgressBar color="bg-blue-500" percentage={90} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<FaGithub className=" text-white text-[40px]" />
 							<ProgressBar color="bg-white" percentage={90} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<FaBootstrap className=" text-purple-700 text-[40px]" />
 							<ProgressBar color="bg-purple-700" percentage={90} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<SiTailwindcss className=" text-cyan-400 text-[40px]" />
 							<ProgressBar color="bg-cyan-400" percentage={90} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<FaWordpress className=" text-cyan-700 text-[40px]" />
 							<ProgressBar color="bg-cyan-700" percentage={90} />
 						</div>
 					</div>
 					<div>
-						<div className="flex max-w-[350px] mx-auto my-4">
+					<div className="flex w-[350px] mx-auto my-4">
 							<Image src={Joomla} alt="" width={40} height={40} className=" rounded" />
 							<ProgressBar color="bg-green-700" percentage={80} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<Image src={Java} alt="" width={40} height={40} className=" rounded" />
 							<ProgressBar color="bg-red-700" percentage={60} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<SiSpring className=" text-green-500 text-[40px]" />
 							<ProgressBar color="bg-green-500" percentage={60} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<FaAngular className=" text-red-500 rounded text-[40px]" />
 							<ProgressBar color="bg-red-500" percentage={60} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<SiTypescript className=" text-blue-800 bg-white rounded text-[40px]" />
 							<ProgressBar color="bg-blue-800" percentage={60} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<Image src={MySql} alt="" width={40} height={40} className="bg-white rounded" />
 							<ProgressBar color="bg-cyan-600" percentage={60} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<Image src={Figma} alt="" width={40} height={40} className=" rounded" />
 							<ProgressBar color="bg-orange-500" percentage={60} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<SiCypress className=" text-green-300 rounded text-[40px]" />
 							<ProgressBar color="bg-green-300" percentage={60} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<SiJest className=" text-red-800 rounded text-[40px]" />
 							<ProgressBar color="bg-red-800" percentage={60} />
 						</div>
 
-						<div className="flex max-w-[350px] mx-auto my-4">
+						<div className="flex w-[350px] mx-auto my-4">
 							<Image src={JUnit5} alt="" width={40} height={40} className="" />
 							<ProgressBar color="bg-red-400" percentage={60} />
 						</div>
