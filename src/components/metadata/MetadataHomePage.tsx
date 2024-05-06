c4fac076ec2faf50d9a43e9b8babde012db40b89
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-05-06 19:55:46 +0200

    metadata ok

---
+++ src/components/metadata/MetadataHomePage.tsx
@@ -0,0 +1,64 @@
+const MetadataHomePage = [
+	{
+		"@context": "http://schema.org",
+		"@type": "WebPage",
+		"@id": "https://www.creation-site-internet.dev/",
+		url: "https://www.creation-site-internet.dev/",
+		name: "Création de Sites Internet sur mesure | Optimisation SEO",
+		description:
+			"Découvrez nos services de création de sites internet, conçus pour dynamiser votre présence en ligne. Expertise en SEO pour améliorer votre visibilité sur Google et attirer un trafic qualifié.",
+		inLanguage: "fr-FR",
+		datePublished: "2018-01-01T00:00:00+01:00",
+		dateModified: "2023-09-30T11:19:14+01:00",
+		openingHoursSpecification: [
+			{
+				"@type": "OpeningHoursSpecification",
+				dayOfWeek: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+				opens: "10:00",
+				closes: "20:00",
+			},
+			{
+				"@type": "OpeningHoursSpecification",
+				dayOfWeek: "Saturday",
+				opens: "10:00",
+				closes: "20:00",
+			},
+		],
+		telephone: "O477131993",
+		email: "stephanegamot@outlook.com",
+
+		sameAs: ["https://www.facebook.com/profile.php?id=100087896780441", "https://twitter.com/whitewo26072430"],
+		image: ["https://www.creation-site-internet.dev/webDevAtWork.jpg"],
+
+		offers: [
+			{
+				"@type": "Offer",
+				url: "https://www.creation-site-internet.dev/web",
+				price: "Contactez-nous pour un devis personnalisé",
+				priceCurrency: "EUR",
+				availability: "http://schema.org/InStock",
+				validFrom: "2024-02-14",
+				category: "https://www.creation-site-internet.dev/web",
+				itemOffered: {
+					"@type": "Service",
+					name: "Création de site web personnalisé",
+				},
+			},
+			{
+				"@type": "Offer",
+				url: "https://www.creation-site-internet.dev/seo-referencement-naturel",
+				price: "Contactez-nous pour un devis personnalisé",
+				priceCurrency: "EUR",
+				availability: "http://schema.org/InStock",
+				validFrom: "2024-02-14",
+				category: "https://www.creation-site-internet.dev/seo-referencement-naturel",
+				itemOffered: {
+					"@type": "Service",
+					name: "Référencement SEO Google",
+				},
+			},
+		],
+	},
+];
+
+export default MetadataHomePage;
