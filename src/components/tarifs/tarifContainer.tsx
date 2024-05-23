798d1cb7cff771cd58df80250432f33a088de446
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-05-23 22:40:01 +0200

    last

--- src/components/tarifs/tarifContainer.tsx
+++ src/components/tarifs/tarifContainer.tsx
@@ -9,14 +9,14 @@ export default function TarifContainer() {
 <h2 className="text-center text-black text-3xl py-5">Tarifs</h2>
 
 <div className="container mx-auto px-4">
-				<h3 className="text-center text-black text-3xl mb-4">Création de site internet avec WordPress</h3>
+				<h3 className="text-center text-black text-3xl mb-4">Creation de site internet avec WordPress</h3>
 				<div className="flex flex-wrap gap-5 justify-around mb-5">
 					<TarifCard key={TarifsData[0].id} title={TarifsData[0].title} price={TarifsData[0].price} developement={TarifsData[0].developement} avantage={TarifsData[0].avantage} />
 					<TarifCard key={TarifsData[1].id} title={TarifsData[1].title} price={TarifsData[1].price} developement={TarifsData[1].developement} avantage={TarifsData[1].avantage} />
 					<TarifCard key={TarifsData[2].id} title={TarifsData[2].title} price={TarifsData[2].price} developement={TarifsData[2].developement} avantage={TarifsData[2].avantage} />
 				</div>
 				<div>
-                <h3 className="text-center text-black text-3xl mb-4">Création de site internet avec React - Next</h3>
+                <h3 className="text-center text-black text-3xl mb-4">Creation de site internet avec React - Next</h3>
                 <div className="flex flex-wrap justify-around gap-5 mb-5">
 						<TarifCard key={TarifsData[3].id} title={TarifsData[3].title} price={TarifsData[3].price} developement={TarifsData[3].developement} avantage={TarifsData[3].avantage} />
 						<TarifCard key={TarifsData[4].id} title={TarifsData[4].title} price={TarifsData[4].price} developement={TarifsData[4].developement} avantage={TarifsData[4].avantage} />
