00bf3cf55fe4c19221c6c7a8488e46c9fbb87804
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-06-09 17:55:37 +0200

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-	return <p className="text-center ">© Copyright 2024 | Stéphane Gamot Web Développeur Front-End React & Next</p>;
+	return <p className="text-center ">© Copyright 2024 | Agence web Stéphane Gamot</p>;
 }
