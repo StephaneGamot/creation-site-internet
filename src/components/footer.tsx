bde2a9741865d14df948cc38eb4b51bc830d3dba
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2024-07-08 13:34:32 +0200

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,13 +5,11 @@ import PhonePic from "./../../public/img/www2.webp";
 
 export default function Footer() {
 	return (
-		<>
-			<p className="text-center ">© Copyright 2024</p>
-			<p className=" text-lg max-w-lg mx-auto text-center mb-2 font-medium sm:mb-4 sm:leading-snug sm:max-w-xl  md:leading-snug md:max-w-3xl">
-				Intéressé ? Je suis à votre disposition pour une
-				<strong className="italic"> collaboration</strong>.
-			</p>
-			<div id="contact"  className="text-main flex mt-0 justify-center items-center flex-col md:flex-row md:justify-center gap-4">
+		<footer className="flex flex-col items-center md:flex-row md:flex-nowrap justify-center gap-5">
+			<div>
+			<p className="text-center mb-0 ">© Copyright 2024</p></div>
+		<div>
+			<div id="contact"  className="text-main flex my-0 justify-center items-center flex-col md:flex-row md:justify-center gap-4">
 			<div className="flex items-center">
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
 						<path
@@ -21,7 +19,7 @@ export default function Footer() {
 						/>
 					</svg>
 					&nbsp;&nbsp;
-					<p className="md:text-lg text-zinc-50"> stephanegamot@outlook.com</p>
+					<p className="my-0 md:text-lg  text-zinc-50"> stephanegamot@outlook.com</p>
 				</div>
 				<div className="flex items-center">
 					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
@@ -31,11 +29,11 @@ export default function Footer() {
 							d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
 						/>
 					</svg> &nbsp;&nbsp;
-					<p className="md:text-lg text-zinc-50">04 77 13 19 93</p>
+					<p className="my-0 md:text-lg text-zinc-50">04 77 13 19 93</p>
 				</div>
 			</div>
 
 			<div></div>
-		</>
+		</div></footer>
 	);
 }
