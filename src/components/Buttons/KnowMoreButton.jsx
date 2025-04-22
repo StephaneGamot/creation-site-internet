61f07f8b723af1a90d7853bf3c89b9af0b841637
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2025-04-22 22:46:22 +0200

    !

--- src/components/Buttons/KnowMoreButton.jsx
+++ src/components/Buttons/KnowMoreButton.jsx
@@ -5,7 +5,7 @@ export default function KnowMoreButton() {
     <Link
       href="/web"
       role="button"
-      className="inline-block bg-[#43986b] text-black px-6 py-3 rounded-xl font-semibold shadow hover:bg-opacity-90 transition focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#43986b]"
+      className="inline-block bg-[#43986b] !text-black px-6 py-3 rounded-xl font-semibold shadow hover:bg-opacity-90 transition focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#43986b]"
       aria-label="Lien permtant d'aller à la page parlant de création internet"
     >
       en savoir plus
