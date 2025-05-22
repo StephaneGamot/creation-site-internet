e95429fa239c7239fc94625c7c3d375c26eb7b86
stephane Gamot <116945810+White-Wolf-Web@users.noreply.github.com>
2025-05-22 22:17:42 +0200

    Create route.js

---
+++ src/app/sitemap.xml/route.js
@@ -0,0 +1,50 @@
+export async function GET() {
+    const baseUrl = 'https://www.creation-site-internet.dev';
+    const locales = ['fr', 'en', 'nl'];
+    const paths = [
+      '', // Home
+      'web',
+      'seo-referencement-naturel',
+      'stephane-gamot/portfolio',
+      'stephane-gamot',
+      'contact',
+    ];
+  
+    const today = new Date().toISOString();
+  
+    const urls = [];
+  
+    for (const path of paths) {
+      for (const locale of locales) {
+        const loc = `${baseUrl}/${locale}/${path}`.replace(/\/+$/, '');
+  
+        const alternateLinks = locales
+          .map(lang => {
+            const href = `${baseUrl}/${lang}/${path}`.replace(/\/+$/, '');
+            return `<xhtml:link rel="alternate" hreflang="${lang}" href="${href}" />`;
+          })
+          .join('\n    ');
+  
+        urls.push(`
+      <url>
+        <loc>${loc}</loc>
+        <lastmod>${today}</lastmod>
+        ${alternateLinks}
+      </url>`);
+      }
+    }
+  
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xhtml="http://www.w3.org/1999/xhtml">
+  ${urls.join('\n')}
+  </urlset>`;
+  
+    return new Response(xml, {
+      headers: {
+        'Content-Type': 'application/xml',
+      },
+    });
+  }
+  
